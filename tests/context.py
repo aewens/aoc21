5fe7b16dd8d77c7382f1bb6ff201367ff32656e0
aewens <email@aewens.com>
2021-11-23 18:57:11 +0000

    Added basic setup for doing test driven development using pytest

---
+++ tests/context.py
@@ -0,0 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).absolute().parent.parent))
+
+from utils import get_puzzle, get_solutions
+
+solutions = get_solutions()
